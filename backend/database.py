958a78164bace23d2aa6cd9087b8c90d52cd4697
adeland <adeland.luo21@sitechhs.com>
2023-09-15 20:48:08 -0400

    added env file to database.py and env to requirements.tx

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,13 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.json_util import dumps
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://adeland:boWpe6PnOM8yu7Xs@cluster0.m1cajrn.mongodb.net/?retryWrites=true&w=majority"
+load_dotenv()
+
+uri = os.getenv("mongodb+srv://adeland:boWpe6PnOM8yu7Xs@cluster0.m1cajrn.mongodb.net/?retryWrites=true&w=majority")
 
 client = MongoClient(uri)
 
