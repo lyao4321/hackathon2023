4564b3575d3b872b998495a34f86cbbf97ef46bf
adeland <adelandluo1@gmail.com>
2023-09-15 20:29:17 -0400

    added ping

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,15 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.json_util import dumps
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://adeland:boWpe6PnOM8yu7Xs@cluster0.m1cajrn.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
