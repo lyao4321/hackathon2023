9f87252c6262d80ee6b611f434078c08ef96f6de
Jason Mai <maijason112102@gmail.com>
2023-09-18 13:02:16 -0400

    map

--- frontend/src/containers/MapComponent.tsx
+++ frontend/src/containers/MapComponent.tsx
@@ -4,11 +4,24 @@ import React, { useEffect, useRef } from 'react';
 
 const RADIUS = 10 * 1609.34; // 50 miles to meters
 
+
+
 const MapComponent: React.FC = () => {
   const mapRef = useRef<HTMLDivElement | null>(null);
   const googleMap = useRef<google.maps.Map>();
   const userMarker = useRef<google.maps.Marker>();
   const userCircle = useRef<google.maps.Circle>();
+  const geocoder = useRef(new google.maps.Geocoder());
+
+  function geocodeAddress(address: string, callback: (results: google.maps.GeocoderResult[]) => void) {
+    geocoder.current.geocode({ 'address': address }, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK && results) {
+        callback(results);
+      } else {
+        console.error("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }  
 
   useEffect(() => {
     if (mapRef.current) {
@@ -29,6 +42,28 @@ const MapComponent: React.FC = () => {
             position: location,
             map: googleMap.current,
           });
+
+          geocodeAddress('247 Cayuga Rd Suite 50, Buffalo, NY 14225', (results) => {
+            new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: googleMap.current,
+            });
+          });
+
+          geocodeAddress('3711 Villas Drive West, Buffalo, NY 14228', (results) => {
+            new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: googleMap.current,
+            });
+          });
+
+          geocodeAddress('403 Main St Ste. 200, Buffalo, NY 14203', (results) => {
+            new google.maps.Marker({
+              position: results[0].geometry.location,
+              map: googleMap.current,
+            });
+          });
+        
 
           userCircle.current = new google.maps.Circle({
             center: location,
